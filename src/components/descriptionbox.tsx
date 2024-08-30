2749c38aa82f40c6f84ee2a6217d12eeca46c407
Sam Ford <samueljamesford@googlemail.com>
2024-08-30 17:47:00 +0100

    Styles & Positions Description Box

--- src/components/descriptionbox.tsx
+++ src/components/descriptionbox.tsx
@@ -7,28 +7,35 @@ const DescriptionBox: React.FC<{ card: Card }> = ({ card }) => {
     ? card.mana_cost.match(/\{(.*?)\}/g)?.map(match => match.slice(1, -1))
     : [];
 
+
+
+
   return (
     <div className='description-container'>
-      <h2>{card.name}</h2>
-      {matches && (
-        <span>
-          Cost:
-          {matches.map((pip, index) => (
-            <Manapip key={index} size={25} colour={pip} />
-          ))}
-          <br />
-        </span>
-      )}
+      <div className='card-desc-line'>
+        <span>{card.name}</span>
+
+        {matches && (
+          <span>
+            {matches.map((pip, index) => (<Manapip key={index} size={25} colour={pip} />))}
+          </span>
+        )}
+      </div>
+      <div className='card-line'>
+            {card.type_line}  <span className={`rarity-${card.rarity}`}></span>
+      </div>
+       
+      
+      {/* Cost */}
+      
+  
 
 
 
-    {card.type_line && <span>Type: {card.type_line} <br/> </span> }
-    {card.power && <span>Power: {card.power} <br/> </span>}
-    {card.toughness && <span>Toughness: {card.toughness} <br/> </span>}
+    {card.power && card.toughness && <span>{card.power} / {card.toughness}<br/> </span>}
     {card.loyalty && <span>Loyalty: {card.loyalty} <br/> </span>}
-    {card.flavor_text && <span>Flavor Text: {card.flavor_text} <br/> </span>}
-    {card.rarity && <span>Rarity: {card.rarity} <br/> </span>}
-    {card.artist && <span>Artist: {card.artist} <br/> </span>}
+    {card.flavor_text && <div className='desc-right-italic'>{card.flavor_text}  </div>}
+    {card.artist && <div className='desc-right-italic'>Artist: {card.artist} </div>}
     </div>
 );
 };

7e2085995680b495b3c9102c17460b0740a5995d
Sam Ford <samueljamesford@googlemail.com>
2024-11-08 14:00:20 +0000

    Adds background/Text Toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import ScryFallCaller from "@/components/scryfallcaller";
 import { useState } from "react";
-import { IoMdText } from "react-icons/io";
+import { TbArticleFilled,TbBackground } from "react-icons/tb";
 
 const sets = {
   kaldheim: {
@@ -19,25 +19,40 @@ const sets = {
 };
 
 export default function Home() {
-const [showCardText,setShowCardText] = useState(false)
-const SelectedSet = sets.foundations;
+  const [showCardText,setShowCardText] = useState(false)
+  const [showBackGround,setShowBackground] = useState(false)
+  const SelectedSet = sets.foundations;
 
 return (
-  <div 
-    className="Homepage-Background"
-    style={{backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4)), url(${SelectedSet.setBackGround})`}}
+  <div
+  className="Homepage-Background"
+  style={{
+    backgroundImage: showBackGround
+      ? `linear-gradient(90deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4)), url(${SelectedSet.setBackGround})`
+      : undefined,
+    backgroundColor: showBackGround ? undefined : 'rgb(0,255,0)'
+  }}
   >
+
     <img className='set-Logo' src={SelectedSet.setLogo} alt={`${SelectedSet.setName} Logo`} />
     
     <div className="carousel-container">
       <ScryFallCaller setCode={SelectedSet.setCode} showCardText={showCardText}/>
     </div>
 
-    <div className="copyright" onClick={()=>setShowCardText(!showCardText)}>
-      <IoMdText
-        className={`reactIcon ${showCardText ? 'ActiveIcon' : 'InactiveIcon'}`}
-        onClick={() => setShowCardText(!showCardText)}
-      />
+    <div className="copyright">
+      <div className="icons-container">
+
+        <TbArticleFilled
+          className={`reactIcon ${showCardText ? 'ActiveIcon' : 'InactiveIcon'}`}
+          onClick={() => setShowCardText(!showCardText)}
+        />
+        <TbBackground
+          className={`reactIcon ${showBackGround ? 'ActiveIcon' : 'InactiveIcon'}`}
+          onClick={() => setShowBackground(!showBackGround)}
+        />
+
+      </div>
       <p>Magic: The Gathering is copyright Wizards of the Coast, LLC. Opt is not produced by or endorsed by Wizards of the Coast.</p>
     </div>
 
